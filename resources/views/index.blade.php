995a733153c58218319df4f04c2a74b658539bc5
sasa888 <majapljeskavica@yahoo.com>
2016-12-14 22:18:45 +0100

    first commit

---
+++ resources/views/index.blade.php
@@ -0,0 +1,75 @@
+@extends('layouts.master')
+
+
+@section('title')
+
+@stop
+@section('styles')	
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+	{{-- <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css"> --}}
+@endsection
+@section('content')
+	@if(!empty(Request::segment(1) ) )
+		<section class="filter-bar">
+			A filter has been set! <a href="{{ route('index') }}">Show all quotes</a>
+		</section>
+	@endif
+
+	@if(count($errors) > 0 )
+		<section class="info-box fail">
+				@foreach($errors->all() as $error)
+					{{ $error }}
+				@endforeach
+		</section>
+	@endif
+	@if(Session::has('success'))
+		<section class="info-box success">
+			{{ Session::get('success') }}
+		</section>
+	@endif
+	<section class="quotes">
+		<h1>Latest Quotes</h1>
+
+			@for($i = 0; $i < count($quotes); $i++)
+
+				<article class="quote{{ $i % 3 === 0  ? ' first-in-line' : (($i + 1) % 3 === 0 ? ' last-in-line' : ' ' ) }}">
+					<div class="delete"><a href="{{ route('delete' , ['quote_id' => $quotes[$i]->id] )  }}">X</a></div>
+						{{ $quotes[$i]->quote }}
+					<div class="info">Created by <a href="{{ route('index' , ['author' => $quotes[$i]->author->name]) }}">{{ $quotes[$i]->author->name }}</a> on {{ $quotes[$i]->created_at }} </div>
+				</article>
+			@endfor
+			<div class="pagination">
+				@if($quotes->currentPage() !==1)
+					<a href="{{ $quotes->previousPageUrl() }}"><span class="fa fa-caret-left"></span></a>
+
+				@endif
+
+				@if( $quotes->currentPage() !==  $quotes->lastPage() && $quotes->hasPages() )
+
+					<a href="{{ $quotes->nextPageUrl() }}"><span class="fa fa-caret-right"></span></a>
+					
+				@endif
+			</div>
+		
+	</section>
+	
+	<section class="edit-quote">
+		<h1>Add a Quote</h1>
+		<form action="{{  route('create')  }}" method="post">
+			<div class="input-group">
+				<label for="author">Your Name</label>
+				<input type="text" name="author" id="author" placeholder="Your Name"> 
+			</div>
+
+			<div class="input-group">
+				<label for="quote">Your Quote</label>
+				<textarea name="quote" id="quote" rows="5" placeholder="Quote"></textarea> 
+			</div>
+
+			<input type="submit" class="btn btn-primary"">
+			<input type="hidden" name="_token" value="{{ Session::token() }}"> 
+		</form>
+		
+	</section>
+
+@stop
